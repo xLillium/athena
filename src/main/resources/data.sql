4882375dd84ae55224c728cf17df572e450fd2c1
xLillium <nicolas.motillon@gmail.com>
2023-10-17 14:43:09 +0200

    Added Book to schema.sql and data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO users (id, first_name, last_name) VALUES (101, 'Alice', 'Alpha');
 INSERT INTO users (id, first_name, last_name) VALUES (102, 'Bob', 'Bravo');
+
+INSERT INTO books (id, title, author, isbn) VALUES (201, '1984', 'George Orwell', '1234567890');
+INSERT INTO books (id, title, author, isbn) VALUES (202, '1985', 'George Orwell', '1234567891');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,4 @@
+-- Users table
 CREATE TABLE public.users (
     id integer NOT NULL,
     first_name character varying(255),
@@ -21,3 +22,29 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+-- Books table
+CREATE TABLE public.books (
+    id integer NOT NULL,
+    title character varying(255),
+    author character varying(255),
+    isbn character varying(13)
+);
+
+ALTER TABLE public.books OWNER TO athena;
+
+CREATE SEQUENCE public.books_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER TABLE public.books_id_seq OWNER TO athena;
+
+ALTER TABLE ONLY public.books ALTER COLUMN id SET DEFAULT nextval('public.books_id_seq'::regclass);
+
+ALTER TABLE ONLY public.books
+    ADD CONSTRAINT books_pkey PRIMARY KEY (id);
