58ab60d701155c2ca687ba52dfb450d092bf4dfb
xLillium <nicolas.motillon@gmail.com>
2023-10-19 16:50:44 +0200

    Refactored BookValidationTest

--- src/test/java/com/nmotillon/athena/validation/BookValidationTest.java
+++ src/test/java/com/nmotillon/athena/validation/BookValidationTest.java
@@ -1,74 +1,88 @@
 package com.nmotillon.athena.validation;
 
 import com.nmotillon.athena.model.Book;
-import com.nmotillon.athena.repository.BookRepository;
-import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.ConstraintViolation;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 
-@DataJpaTest
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
 public class BookValidationTest {
     @Autowired
-    BookRepository bookRepository;
+    private LocalValidatorFactoryBean validator;
+    private Book book;
 
-    @Test
-    public void savingBookWithNullTitleShouldThrowValidationException() {
-        Book book = new Book(null, "Sample Author", "1234567890");
-        assertThrows(ConstraintViolationException.class, () -> bookRepository.save(book));
+    @BeforeEach
+    public void setUp() {
+        book = new Book("Sample Title", "Sample Author", "1234567890123");
     }
 
     @Test
-    public void savingBookWithNullAuthorShouldThrowValidationException() {
-        Book book = new Book("Sample Title", null, "1234567890");
-        assertThrows(ConstraintViolationException.class, () -> bookRepository.save(book));
+    public void whenValidData_thenNoViolations() {
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).isEmpty();
     }
 
     @Test
-    public void savingBookWithNullIsbnShouldThrowValidationException() {
-        Book book = new Book("Sample Title", "Sample Author", null);
-        assertThrows(ConstraintViolationException.class, () -> bookRepository.save(book));
+    public void whenNullTitle_thenViolation() {
+        book.setTitle(null);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).isNotEmpty();
+        assertThat(violations).extracting("message").containsExactly("Title is mandatory");
     }
 
     @Test
-    public void savingBookWithIsbnLengthOf10_ShouldBeValid() {
-        Book book = new Book("Sample Title", "Sample Author", "1234567890");
-        Book savedBook = bookRepository.save(book);
-
-        Optional<Book> retrievedBook = bookRepository.findById(savedBook.getId());
-
-        assertTrue(retrievedBook.isPresent());
-        assertEquals("1234567890", retrievedBook.get().getIsbn());
+    public void whenNullAuthor_thenViolation() {
+        book.setAuthor(null);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).isNotEmpty();
+        assertThat(violations).extracting("message").containsExactly("Author is mandatory");
     }
 
     @Test
-    public void savingBookWithIsbnLengthOf13_ShouldBeValid() {
-        Book book = new Book("Sample Title", "Sample Author", "1234567890123");
-        Book savedBook = bookRepository.save(book);
-
-        Optional<Book> retrievedBook = bookRepository.findById(savedBook.getId());
-
-        assertTrue(retrievedBook.isPresent());
-        assertEquals("1234567890123", retrievedBook.get().getIsbn());
+    public void whenNullIsbn_thenViolation() {
+        book.setIsbn(null);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).isNotEmpty();
+        assertThat(violations).extracting("message").containsExactly("ISBN is mandatory");
     }
 
     @Test
-    public void savingBookWithIsbnLengthOf11_ShouldThrowValidationException() {
-        Book book = new Book("Sample Title", "Sample Author", "12345678901");
-        // Expect a validation exception to be thrown
-        assertThrows(ConstraintViolationException.class, () -> bookRepository.save(book));
+    public void whenIsbnLengthOf10_thenValid() {
+        book.setIsbn("1234567890");
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).isEmpty();
     }
 
     @Test
-    public void savingBookWithIsbnLengthOf12_ShouldThrowValidationException() {
-        Book book = new Book("Sample Title", "Sample Author", "123456789012");
-        // Expect a validation exception to be thrown
-        assertThrows(ConstraintViolationException.class, () -> bookRepository.save(book));
+    public void whenIsbnLengthOf13_thenValid() {
+        book.setIsbn("1234567890123");
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).isEmpty();
+    }
+
+    @Test
+    public void whenIsbnLengthOf11_thenViolation() {
+        book.setIsbn("12345678901");
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).isNotEmpty();
+        assertThat(violations).extracting("message").containsExactly("ISBN should be either 10 or 13 digits");
+    }
+
+    @Test
+    public void whenIsbnLengthOf12_thenViolation() {
+        book.setIsbn("123456789012");
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).isNotEmpty();
+        assertThat(violations).extracting("message").containsExactly("ISBN should be either 10 or 13 digits");
     }
 
 }
